eb34492f8e2af42c2dfead7638445f1bc1764416
cergmin <cergmin@gmail.com>
2020-11-02 17:38:13 +1000

    Изменена система добавления тестов
    Теперь группы и тесты добавляются двумя разными методами, возвращаются id новых сущностей

--- controllers.py
+++ controllers.py
@@ -34,17 +34,8 @@ class SQLController:
             WHERE parent = ?""", [parent]).fetchall()
     
     def add_test(self, title='', subtitle='', input_type=0, output_type=0, 
-                 input_val='', output_val='', parent=-1, is_group=False):
+                 input_val='', output_val='', parent=-1):
         cur = self.con.cursor()
-        
-        if is_group:
-            title = ''
-            subtitle = ''
-            input_type = 0
-            output_type = 0
-            input_val = ''
-            output_val = ''
-            parent = -1
         
         if parent != -1:
             results = cur.execute("""SELECT COUNT(*) FROM tests
@@ -56,11 +47,23 @@ class SQLController:
         cur.execute("""INSERT 
             INTO tests(title, subtitle, input_type, output_type, 
                 input, output, parent, is_group) 
-            VALUES(?, ?, ?, ?, ?, ?, ?, ?)""", 
+            VALUES(?, ?, ?, ?, ?, ?, ?, false)""", 
             [title, subtitle, input_type, output_type, 
-            input_val, output_val, parent, is_group])
+            input_val, output_val, parent])
 
         self.con.commit()
+
+        return cur.lastrowid
+    
+    def add_group(self):
+        cur = self.con.cursor()
+
+        cur.execute("""INSERT 
+            INTO tests(is_group) 
+            VALUES(true)""")
+        self.con.commit()
+        
+        return cur.lastrowid
     
     def set_verdict(self, id_val, verdict):
         cur = self.con.cursor()
@@ -80,6 +83,9 @@ class SQLController:
             WHERE id = ?""", [verdict, id_val])
 
         self.con.commit()
+
+        results = cur.execute("""SELECT parent FROM tests
+            WHERE id = ?""", [id_val]).fetchone()
     
     def set_console_output(self, id_val, console_output):
         cur = self.con.cursor()
